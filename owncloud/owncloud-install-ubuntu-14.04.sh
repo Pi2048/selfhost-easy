d4cade0cbeeb22dfb8a90e958a1dbf221b910318
Pieter <@>
2016-03-21 22:16:06 +0100

    Add OwnCloud script

---
+++ owncloud/owncloud-install-ubuntu-14.04.sh
@@ -0,0 +1,9 @@
+echo 'deb http://download.owncloud.org/download/repositories/stable/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud.list
+cd
+wget -nv https://download.owncloud.org/download/repositories/stable/xUbuntu_14.04/Release.key -O Release.key
+apt-key add - < Release.key
+rm Release.key
+apt-get update
+apt-get install owncloud
+ln -s /var/www/owncloud /usr/share/owncloud
+service apache2 reload
