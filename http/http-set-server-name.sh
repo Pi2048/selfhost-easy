c401df3665b26212b235cc28e65464076401d92f
Pieter <@>
2016-03-21 13:44:10 +0100

    Fix HTTP server name script

--- http/http-set-server-name.sh
+++ http/http-set-server-name.sh
@@ -1,2 +1,3 @@
 expression="s/#ServerName.*$/ServerName $1/"
-cat /etc/apache2/sites-available/000-default.conf | sed -e "$expression" > /root/output
+sed "$expression" -i /etc/apache2/sites-available/000-default.conf
+service apache2 reload
